99b7f4440d560234ed5b12b16b4e0bfb1b778bf0
Jason Offutt <jason.offutt@gmail.com>
2011-04-24 22:42:49 -0700

    Adding singleton cache implementation to add some extra speed to repository layer.

---
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Framework/Helpers/SingletonCace.cs
@@ -0,0 +1,36 @@
+﻿//
+// Copyright © 2011 Jordan Rift, LLC - All Rights Reserved
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+using System.Runtime.Caching;
+
+namespace JordanRift.Grassroots.Framework.Helpers
+{
+    /// <summary>
+    /// Singleton implementation to act as a wrapper for a MemoryCache object.
+    /// </summary>
+    public class SingletonCache
+    {
+        // Lazy<T> in this usage does act as a thread-safe instance of the generic type. No need for "volatile".
+        private static readonly Lazy<SingletonCache> instance = new Lazy<SingletonCache>(() => new SingletonCache());
+
+        public static SingletonCache Instance
+        {
+            get { return instance.Value; }
+        }
+
+        private MemoryCache cache;
+
+        public MemoryCache Cache
+        {
+            get { return cache ?? (cache = MemoryCache.Default); }
+        }
+
+        private SingletonCache() { }
+    }
+}
