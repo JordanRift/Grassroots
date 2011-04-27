92cefe2ca658d82dab32e7e16db34177465352d6
Jason Offutt <jason.offutt@gmail.com>
2011-04-26 20:19:24 -0700

    Fleshing out base style rules for forms.

--- trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Helpers/HtmlHelperExtensions.cs
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Helpers/HtmlHelperExtensions.cs
@@ -7,7 +7,10 @@
 //
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 using System.Web.Mvc;
 
 namespace JordanRift.Grassroots.Web.Helpers
@@ -29,17 +32,32 @@ namespace JordanRift.Grassroots.Web.Helpers
         /// Html Helper method to render out a cancel button with jQuery UI classes
         /// </summary>
         /// <param name="helper">HtmlHelper class to add extension to</param>
-        /// <param name="text">Display text to render to the button</param>
+        /// <param name="value">Display text to render to the button</param>
         /// <returns>Html representation of a cancel button</returns>
         public static MvcHtmlString UiCancel(this HtmlHelper helper, string value = "Cancel")
         {
             return new MvcHtmlString(string.Format("<button id=\"cancel\" class=\"ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only\">{0}</button>", value));
         }
 
-        //public static MvcHtmlString UiValidationFor<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
+        //public static MvcHtmlString UiValidationFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
         //{
-        //    var result = expression.Compile();
-        //    result.Invoke();
+        //    var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+        //    var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+        //    var errorText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+
+        //    if (string.IsNullOrEmpty(errorText))
+        //    {
+        //        return MvcHtmlString.Empty;
+        //    }
+
+        //    var html = new StringBuilder();
+        //    html.AppendLine("<div class=\"ui-widget field-validation-valid\">");
+        //    html.AppendLine("<div class=\"ui-state-error ui-corner-all\" style=\"padding: 0 .7em;\">");
+        //    html.AppendLine("<span class=\"ui-icon ui-icon-alert\" style=\"float: left; margin-right: .3em;\"></span>");
+        //    html.AppendLine("<span data-valmsg-replace=\"true\" data-valmsg-for=\"" + metadata.PropertyName + "\">" + errorText + "</span>");
+        //    html.AppendLine("</div>");
+        //    html.AppendLine("</div>");
+        //    return new MvcHtmlString(html.ToString());
         //}
     }
 }
