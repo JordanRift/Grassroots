3b0630f1de3fd52ef80d9cff56f0ed704021d762
Jason Offutt <jason.offutt@gmail.com>
2011-04-26 17:35:47 -0700

    Updating packages. Tweaking javascript behavior. Updating jQuery and jQuery Validate.

--- trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Scripts/grassroots.core.js
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Scripts/grassroots.core.js
@@ -8,6 +8,30 @@
 // http://creativecommons.org/licenses/by-nc-sa/3.0/
 //
 
+Grassroots = {};
+
+Grassroots.progressbar = function () {
+    var $progressbar = $(".ui-progressbar-value");
+
+    if ($progressbar.length > 0) {
+        var px = $progressbar.outerWidth();
+        $progressbar.css("width", "0px").show().animate({ width: '+=' + px + 'px' }, 2000);
+        $(".progress-indicator").animate({ left: '+=' + px + 'px', opacity: 1 }, 2000);
+    }
+}
+
+//GrassrootsEvents = [];
+
+//Configure RequireJS
+//require({ priority: ['jquery-1.4.4.min'] },
+//    ['jquery-1.4.4.min', 'modernizr-1.7.min', 'grassroots.ui'],
+//    function () {
+//        for (var i = 0; i < GrassrootsEvents.length; i++) {
+//            eval(GrassrootsEvents[i]);
+//        }
+//    }
+//);
+
 /// <summary>
 /// Since the official Tweet button doesn't support SSL, we'll roll our own for now.
 /// </summary>
