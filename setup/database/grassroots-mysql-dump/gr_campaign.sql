88b43ccfaf283f4010e0e20c2d6fa1ebd47e326a
Jason Offutt <jason.offutt@gmail.com>
2011-05-04 10:02:26 -0700

    Adding MySQL dump files.

---
+++ setup/database/grassroots-mysql-dump/gr_campaign.sql
@@ -0,0 +1,27 @@
+﻿USE grassroots;
+
+CREATE TABLE `gr_campaign` (
+  `CampaignID` int(11) NOT NULL AUTO_INCREMENT,
+  `OrganizationID` int(11) NOT NULL,
+  `UserProfileID` int(11) NOT NULL,
+  `CauseTemplateID` int(11) NOT NULL,
+  `CauseID` int(11) DEFAULT NULL,
+  `Title` varchar(100) NOT NULL DEFAULT '''''',
+  `Description` varchar(2000) NOT NULL DEFAULT '''''',
+  `ImagePath` varchar(100) NOT NULL DEFAULT '''''',
+  `StartDate` datetime NOT NULL DEFAULT '1900-01-01 00:00:00',
+  `EndDate` datetime NOT NULL DEFAULT '1900-01-01 00:00:00',
+  `GoalAmount` decimal(11,2) NOT NULL DEFAULT '0.00',
+  `UrlSlug` varchar(30) NOT NULL DEFAULT '''''',
+  PRIMARY KEY (`CampaignID`),
+  KEY `Campaign_Organization_FK` (`OrganizationID`),
+  KEY `Campaign_UserProfile_FK` (`UserProfileID`),
+  KEY `Campaign_Cause_FK` (`CauseID`),
+  KEY `Campaign_CauseTemplate_FK` (`CauseTemplateID`),
+  CONSTRAINT `Campaign_CauseTemplate_FK` FOREIGN KEY (`CauseTemplateID`) REFERENCES `gr_causetemplate` (`CauseTemplateID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `Campaign_Cause_FK` FOREIGN KEY (`CauseID`) REFERENCES `gr_cause` (`CauseID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `Campaign_Organization_FK` FOREIGN KEY (`OrganizationID`) REFERENCES `gr_organization` (`OrganizationID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `Campaign_UserProfile_FK` FOREIGN KEY (`UserProfileID`) REFERENCES `gr_userprofile` (`UserProfileID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
