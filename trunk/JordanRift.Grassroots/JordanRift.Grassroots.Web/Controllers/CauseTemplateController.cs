d54b6130c8040b1e99fb12be4c2835f2ae5f8cfd
Jason Offutt <jason.offutt@gmail.com>
2011-06-06 09:27:41 -0700

    Adding CauseTempalte controller and associated views stubs.

---
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Controllers/CauseTemplateController.cs
@@ -0,0 +1,39 @@
+﻿//
+// Grassroots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Grassroots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Grassroots.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JordanRift.Grassroots.Web.Controllers
+{
+    public class CauseTemplateController : Controller
+    {
+        //
+        // GET: /CauseTemplate/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id = -1)
+        {
+            return View();
+        }
+    }
+}
