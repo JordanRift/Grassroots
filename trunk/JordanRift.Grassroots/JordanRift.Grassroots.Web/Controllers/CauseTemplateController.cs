23f164bf6f00f5c2d7bf51f21ecc5469aa37fa35
Jason Offutt <jason.offutt@gmail.com>
2011-06-24 12:03:26 -0700

    Buliding out Cause Details markup.

--- trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Controllers/CauseTemplateController.cs
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Web/Controllers/CauseTemplateController.cs
@@ -66,7 +66,7 @@ namespace JordanRift.Grassroots.Web.Controllers
             return View(model);
         }
 
-        //[OutputCache(Duration = 150, VaryByParam = "id")]
+        [OutputCache(Duration = 150, VaryByParam = "id")]
         public ActionResult Search(int id = -1)
         {
             var causeTemplate = causeTemplateRepository.GetCauseTemplateByID(id);
