3e9860057ffebd6aa8eb6732e434822306a804b6
Jason Offutt <jason.offutt@gmail.com>
2011-07-15 17:24:43 -0700

    Adding unit tests for HomeController.

--- trunk/JordanRift.Grassroots/JordanRift.Grassroots.Tests/UnitTests/Controllers/HomeControllerTests.cs
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Tests/UnitTests/Controllers/HomeControllerTests.cs
@@ -13,13 +13,90 @@
 // along with Grassroots.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System.Web.Mvc;
+using JordanRift.Grassroots.Framework.Services;
+using JordanRift.Grassroots.Web.Controllers;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace JordanRift.Grassroots.Tests.UnitTests.Controllers
 {
     [TestFixture]
     public class HomeControllerTests
     {
+        private HomeController controller;
+        private ITwitterService twitterService;
+        private IBlogService blogService;
+        private MockRepository mocks;
 
+        [SetUp]
+        public void SetUp()
+        {
+            mocks = new MockRepository();
+            SetUpController();
+        }
+
+        [Test]
+        public  void Index_Should_Return_View()
+        {
+            var result = controller.Index();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void About_Should_Return_View()
+        {
+            var result = controller.About();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void Terms_Should_Return_View()
+        {
+            var result = controller.Terms();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void ProgressBar_Should_Return_View()
+        {
+            var result = controller.ProgressBar();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void Stats_Should_Return_View()
+        {
+            var result = controller.Stats();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void TwitterFeed_Should_Return_View()
+        {
+            var result = controller.TwitterFeed();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void BlogRssFeed_Should_Return_View()
+        {
+            var result = controller.BlogRssFeed();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test]
+        public void ThemeCss_Should_Return_View()
+        {
+            var result = controller.ThemeCss();
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        private void SetUpController()
+        {
+            twitterService = mocks.DynamicMock<ITwitterService>();
+            blogService = mocks.DynamicMock<IBlogService>();
+            controller = new HomeController(twitterService, blogService);
+        }
     }
 }
