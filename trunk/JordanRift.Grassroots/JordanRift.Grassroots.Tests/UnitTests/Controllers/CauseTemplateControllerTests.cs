3ddd5c639d151487f80ee03e81c380d61e16cd01
Jason Offutt <jason.offutt@gmail.com>
2011-07-18 08:35:44 -0700

    Stubbing out test class for CauseTemplateController.

---
+++ trunk/JordanRift.Grassroots/JordanRift.Grassroots.Tests/UnitTests/Controllers/CauseTemplateControllerTests.cs
@@ -0,0 +1,40 @@
+﻿//
+// Grassroots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Grassroots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Grassroots.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JordanRift.Grassroots.Tests.Fakes;
+using NUnit.Framework;
+
+namespace JordanRift.Grassroots.Tests.UnitTests.Controllers
+{
+    [TestFixture]
+    public class CauseTemplateControllerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            FakeCauseTemplateRepository.Reset();
+        }
+    }
+}
